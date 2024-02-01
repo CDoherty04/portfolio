b209cf46361a50658d83510973dbc82c95e3e005
Charlie Doherty <charlieedoherty@gmail.com>
2024-02-01 12:30:02 -0600

    Render the data and format it

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -16,3 +17,22 @@ with col2:
 content2 = ("Below you can find some of the projects I have created and/or am working on."
             " Feel free to contact me about any of them!")
 st.write(content2)
+
+st.markdown("***")
+
+col3, col4 = st.columns(2)
+df = pandas.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.image("images\\" + row["image"])
+        st.subheader(row["description"])
+        st.info(row["url"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.image("images\\" + row["image"])
+        st.subheader(row["description"])
+        st.info(row["url"])
+
