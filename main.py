64b60e226ea711144f7bd24b387c7a0e6aea5480
Charlie Doherty <charlieedoherty@gmail.com>
2024-02-01 23:24:03 -0600

    More formatting changes like a separator column and better links

--- main.py
+++ main.py
@@ -20,19 +20,18 @@ st.write(content2)
 
 st.markdown("***")
 
-col3, col4 = st.columns(2)
+col3, seperator_col, col4 = st.columns([1.5, .5, 1.5])
 df = pandas.read_csv("data.csv", sep=";")
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
         st.image("images\\" + row["image"])
         st.subheader(row["description"])
-        st.info(row["url"])
+        st.info(f"[Source Code](row['url'])")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
         st.image("images\\" + row["image"])
         st.subheader(row["description"])
-        st.info(row["url"])
-
+        st.info(f"[Source Code](row['url'])")
