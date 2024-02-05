2da3f8208693fca0e46901763b9cb1e40ea5ca1b
Charlie Doherty <charlieedoherty@gmail.com>
2024-02-05 00:21:58 -0600

    Connect the send_email script to the app

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,7 +1,18 @@
 import streamlit as st
+from send_email import send_email
+
 st.set_page_config(page_title="Charlie Doherty")
 
 with st.form(key="email_form"):
-    st.text_input("Enter Email", placeholder="your_email@gmail.com")
-    st.text_area("Enter Message", placeholder="I'd like to set up an interview with you!")
-    st.form_submit_button("Submit")
+    email = st.text_input("Enter Email", placeholder="your_email@gmail.com", key="email_area")
+    message = st.text_area("Enter Message", placeholder="Hi, I'd like to connect with you!", key="message_area")
+    button = st.form_submit_button("Submit")
+    message = f"""\
+Subject: Email From Portfolio Webapp
+{message}
+
+From: {email}
+"""
+    if button:
+        send_email(f"{message}")
+        st.info("Your email was sent successfully!")
--- send_email.py
+++ send_email.py
@@ -1,24 +1,17 @@
 import smtplib
 import ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-user_name = "charlieedoherty@gmail.com"
-password = "wgfs rxro fobp pffv"
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
 
-context = ssl.create_default_context()
-receiver = "charlieedoherty@gmail.com"
-message = """\
-Subject: Subject
+    user_name = "charlieedoherty@gmail.com"
+    password = "wgfs rxro fobp pffv"
 
-Hi Charlie
+    context = ssl.create_default_context()
+    receiver = "charlieedoherty@gmail.com"
 
-Interview tomorrow?
-
-Thanks, Me
-"""
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(user_name, password)
-    server.sendmail(user_name, receiver, message)
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(user_name, password)
+        server.sendmail(user_name, receiver, message)
